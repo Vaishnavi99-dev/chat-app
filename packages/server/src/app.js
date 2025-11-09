a820fea3fff7aa07e4e46f633bae41ca6660bd56
Vaishnavi99-dev <sharmavaishnavi7860@gmail.com>
2025-11-09 17:49:30 +0530

    Initial Server Setup | Github Action (#5)
    Co-authored-by: Vaishnavi99-dev <sharmavaishnavi7860@gmil.com>

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import factorial from './utils.js';
   
 const app = express();
 
@@ -6,8 +7,25 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Chat App Server!');
 });
 
-app.use((req, res) => {
-  res.status(404).send('Route not found');
+// Route for number 
+
+app.get('/:num', (req, res) => {
+  const num = Number(req.params.num);
+
+  if (isNaN(num)) {
+    return res.status(400).send('Please provide a valid number in the URL, e.g. 5');
+  }
+
+  if(num > 6) {
+    return  res.status(400).send('Please provide a number less than or equal to 6');
+  }
+
+  try {
+    const result = factorial(num);
+    res.send(`The factorial of ${num} is ${result}`);
+  } catch (error) {
+    res.status(400).send(error.message);
+  } 
 });
 
 export default app;
--- packages/server/src/tests/app.test.js
+++ packages/server/src/tests/app.test.js
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { server } from '../index.js';
 
+// This is Inte
 describe('Basic Server Test', () => {
   afterAll((done) => {
     server.close(done);
@@ -11,4 +12,29 @@ describe('Basic Server Test', () => {
     expect(res.status).toBe(200);
     expect(res.text).toBe('Welcome to the Chat App Server!');
   });
+
+  it('Should respond to GET /yash with a bad Request', async () => {
+    const res = await request(server).get('/yash');
+    expect(res.status).toBe(400);
+    expect(res.text).toBe('Please provide a valid number in the URL, e.g. 5');
+  });
+
+  it('Should respond to GET /7 with a bad Request', async () => {
+    const res = await request(server).get('/7');
+    expect(res.status).toBe(400);
+    expect(res.text).toBe('Please provide a number less than or equal to 6');
+  });
+
+  it('should return the factorial of 5 when calling GET /5', async () => {
+    const res = await request(server).get('/5');
+    expect(res.status).toBe(200);
+    expect(res.text).toBe('The factorial of 5 is 120');
+  });
+
+  it('Should respond to GET /-4 with a bad Request', async () => {
+    const res = await request(server).get('/-4');
+    expect(res.status).toBe(400);
+    expect(res.text).toBe('Negative numbers do not have factorials.');
+  });
+
 });
